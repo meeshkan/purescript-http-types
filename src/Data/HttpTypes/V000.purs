8b35a9fbedaa02286c0c45e08171fb9028a86cd3
Mike Solomon <mike@meeshkan.com>
2020-07-04 22:15:43 +0300

    Adds generics and newtype

--- src/Data/HttpTypes/V000.purs
+++ src/Data/HttpTypes/V000.purs
@@ -29,7 +29,12 @@ newtype HttpTypesMap b
 
 derive instance newtypeHttpTypesMap :: Newtype (HttpTypesMap a) _
 
+derive instance genericHttpTypesMap :: Generic (HttpTypesMap a) _
+
 derive newtype instance eqHttpTypesMap :: (Eq a) => Eq (HttpTypesMap a)
+
+instance httpTypesMapShow :: (Show a) => Show (HttpTypesMap a) where
+  show = genericShow
 
 data Method
   = GET
@@ -102,6 +107,13 @@ data Query
   = StringQuery String
   | ArrayQuery (Array String)
 
+derive instance genericQuery :: Generic Query _
+
+derive instance eqQuery :: Eq Query
+
+instance showQuery :: Show Query where
+  show = genericShow
+
 instance readForeignQuery :: ReadForeign Query where
   readImpl a = (StringQuery <$> readString a) <|> (ArrayQuery <$> (readArray a >>= sequence <<< map readImpl))
 
@@ -112,6 +124,13 @@ instance writeForeignQuery :: WriteForeign Query where
 data Header
   = StringHeader String
   | ArrayHeader (Array String)
+
+derive instance genericHeader :: Generic Header _
+
+derive instance eqHeader :: Eq Header
+
+instance showHeader :: Show Header where
+  show = genericShow
 
 instance readForeignHeader :: ReadForeign Header where
   readImpl a = (StringHeader <$> readString a) <|> (ArrayHeader <$> (readArray a >>= sequence <<< map readImpl))
@@ -134,6 +153,15 @@ newtype Request
   , timestamp :: Maybe String
   }
 
+derive instance genericRequest :: Generic Request _
+
+derive instance newtypeRequest :: Newtype Request _
+
+derive instance eqRequest :: Eq Request
+
+instance showRequest :: Show Request where
+  show = genericShow
+
 derive newtype instance readForeignRequest :: ReadForeign Request
 
 derive newtype instance writeForeignRequest :: WriteForeign Request
@@ -150,6 +178,15 @@ derive newtype instance readForeignResponse :: ReadForeign Response
 
 derive newtype instance writeForeignResponse :: WriteForeign Response
 
+derive instance genericResponse :: Generic Response _
+
+derive instance newtypeResponse :: Newtype Response _
+
+derive instance eqResponse :: Eq Response
+
+instance showResponse :: Show Response where
+  show = genericShow
+
 newtype Exchange
   = Exchange
   { meta :: Maybe JSON
@@ -160,6 +197,15 @@ newtype Exchange
 derive newtype instance readForeignExchange :: ReadForeign Exchange
 
 derive newtype instance writeForeignExchange :: WriteForeign Exchange
+
+derive instance genericExchange :: Generic Exchange _
+
+derive instance newtypeExchange :: Newtype Exchange _
+
+derive instance eqExchange :: Eq Exchange
+
+instance showExchange :: Show Exchange where
+  show = genericShow
 
 instance readForeignHttpTypesMap :: (ReadForeign a) => ReadForeign (HttpTypesMap a) where
   readImpl f = do
@@ -190,3 +236,10 @@ instance writeForeignJSON :: WriteForeign JSON where
   writeImpl (JNumber j) = writeImpl j
   writeImpl (JString j) = writeImpl j
   writeImpl JNull = writeImpl (null :: Nullable Int) -- chose Int at random
+
+derive instance genericJSON :: Generic JSON _
+
+derive instance eqJSON :: Eq JSON
+
+instance showJSON :: Show JSON where
+  show a = genericShow a
